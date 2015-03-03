e7ad772e854a9839036e6f9fe77cc64b2011f66d
reijuc12 <reijuc12@htl-kaindorf.at>
2015-03-03 14:59:41 +0100

    Commit by Julian

--- src/klassen/Music.java
+++ src/klassen/Music.java
@@ -6,6 +6,9 @@
 package klassen;
 
 import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Random;
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.AudioDevice.*;
@@ -35,7 +38,8 @@ public class Music
   {
     String path="C:\\Users\\Christian\\Desktop\\TheGame\\src\\sounds\\blabla\\CanonInD.mp3";
 //    String path=System.getProperty("user.dir")+File.separator+"sounds"+File.separator+"blabla"+File.separator+"CanonInD.mp3";
-    MusicPlayer mp = new MusicPlayer(path);
+      //URL path = this.getClass().getResource("./src/sounds/PlayerSounds/Fireshot.mp3");
+    MusicPlayer mp = new MusicPlayer(path.toString());
     Thread t=new Thread(mp);
     t.start();
   }
@@ -44,7 +48,7 @@ public class Music
 //    String path=System.getProperty("user.dir")+File.separator+"sounds"+File.separator+"blabla"+File.separator+"Grassstep_"+(r.nextInt(5)+1)+".mp3";
     if(randomGrasStep==null||!randomGrasStep.isAlive())
     {
-      String path="C:\\Users\\Christian\\Desktop\\TheGame\\src\\sounds\\PlayerSounds\\Grassstep_"+(r.nextInt(5)+1)+".mp3";
+      String path="../src/sounds/PlayerSounds/Grassstep_"+(r.nextInt(5)+1)+".mp3";
       MusicPlayer mp = new MusicPlayer(path);
       Thread t=new Thread(mp);
       randomGrasStep=t;
